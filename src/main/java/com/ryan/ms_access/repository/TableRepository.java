0eca2726c462c0fa4826938c0570b7edcb9ea08d
ryan Kim <ryangwooz@gmail.com>
2024-07-26 15:26:02 +0900

    - spotless 의존성 추가 - 테스트 rest 추가

--- src/main/java/com/ryan/ms_access/repository/TableRepository.java
+++ src/main/java/com/ryan/ms_access/repository/TableRepository.java
@@ -1,11 +1,12 @@
 package com.ryan.ms_access.repository;
 
 import com.ryan.ms_access.domain.Table1;
-
 import java.util.List;
 
 public interface TableRepository {
-    void save(Table1 table);
-    Table1 saveAndReturn(Table1 table1);
-    List<Table1> findAll();
+  void save(Table1 table);
+
+  Table1 saveAndReturn(Table1 table1);
+
+  List<Table1> findAll();
 }
--- src/main/java/com/ryan/ms_access/service/Table1Service.java
+++ src/main/java/com/ryan/ms_access/service/Table1Service.java
@@ -2,25 +2,25 @@ package com.ryan.ms_access.service;
 
 import com.ryan.ms_access.domain.Table1;
 import com.ryan.ms_access.repository.TableRepositoryImpl;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class Table1Service {
 
-    private final TableRepositoryImpl tableRepository;
+  private final TableRepositoryImpl tableRepository;
 
-    public void saveTable(Table1 table){
-        tableRepository.save(table);
-    }
+  public void saveTable(Table1 table) {
+    tableRepository.save(table);
+  }
 
-    public Table1 saveTableAndReturn(Table1 table){
-        return tableRepository.saveAndReturn(table);
-    }
-    public List<Table1> getAllTable(){
-        return tableRepository.findAll();
-    }
+  public Table1 saveTableAndReturn(Table1 table) {
+    return tableRepository.saveAndReturn(table);
+  }
+
+  public List<Table1> getAllTable() {
+    return tableRepository.findAll();
+  }
 }
